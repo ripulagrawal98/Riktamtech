ffb8c3fcaa1a0e69ffac53e2dc31bc3229510c8c
Ripul Agrawal <ripul98@outlook.com>
2022-12-20 02:02:13 +0530

    db changes

--- dbconn.py
+++ dbconn.py
@@ -1,10 +1,18 @@
 import mysql.connector
 
+# dbConn = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     password="root@123",
+#     database="Riktamtech"
+# )
+
 dbConn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root@123",
-    database="Riktamtech"
+    host="mysql-101015-0.cloudclusters.net",
+    port="10132",
+    user="admin",
+    password="CtA4GXZY",
+    database="riktamtech"
 )
 
 mySql = dbConn.cursor()
